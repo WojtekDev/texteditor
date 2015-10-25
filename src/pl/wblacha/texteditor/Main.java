60a917c86ae845d79480f47284e2b8c7a976369c
WojtekDev <-@10.2.96.10>
2015-10-25 23:16:39 +0100

    #3 Added window with a title

--- src/pl/wblacha/texteditor/Main.java
+++ src/pl/wblacha/texteditor/Main.java
@@ -1,9 +1,28 @@
 package pl.wblacha.texteditor;
 
+import javax.swing.JFrame;
+
+import pl.wblacha.texteditor.classes.TextEditor;
+
 public class Main {
-
+    /**
+     * Create and show the application's GUI. For thread safety,
+     * this method should be invoked from the event-dispatching thread.
+     */
+    private static void createAndShowTextEditor() {
+        //Create and set up the window.
+        JFrame frame = new TextEditor();
+        //Display the window.
+        frame.setVisible(true);
+    }
+        
     public static void main(String[] args) {
-
+        //Schedule a job for the event-dispatching thread
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowTextEditor();
+            }
+        });
     }
 
 }
---
+++ src/pl/wblacha/texteditor/classes/TextEditor.java
@@ -0,0 +1,11 @@
+package pl.wblacha.texteditor.classes;
+
+import javax.swing.*;
+
+public class TextEditor extends JFrame {
+        
+    public TextEditor() {
+        setTitle("Text editor - developed by Wojciech Blacha 2015");
+        setSize(600, 600);
+    }
+}
